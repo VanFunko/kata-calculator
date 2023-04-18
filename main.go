079491f17da9600296f6ed640dedbab311b6a4c1
Blog Luck <alexfallenearth@gmail.com>
2023-04-18 21:07:32 +0300

    Обновил функцию defineOperandsAndOperator

--- main.go
+++ main.go
@@ -7,63 +7,43 @@ import (
 	"strings"
 )
 
+var availbleOperators = []string{"+", "-", "*", "/"}
+
 func main() {
 	for {
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
 		input := scanner.Text()
 		a, b, operator := defineOperandsAndOperator(input)
-		fmt.Printf("%v %v %v", a, operator, b)
+		fmt.Printf("%v %v %v\n", a, operator, b)
 	}
 
-	fmt.Println("Test gitlab")
 }
+
+//func calculate (a, b int, operator string, isRoman bool) int {
+//
+//}
 
 func defineOperandsAndOperator(input string) (operand1, operand2, operator string) {
 	arr := strings.Split(strings.ReplaceAll(input, " ", ""), "")
 	isOperator := false
 	for i := 0; i < len(arr); i++ {
-		switch arr[i] {
-		case "+":
-			operator = arr[i]
-			arr = strings.Split(strings.Join(arr, ""), arr[i])
-			if len(arr) == 2 {
-				operand1 = arr[0]
-				operand2 = arr[1]
-			} else {
-				panic("Ошибка! Введите 2 операнда и 1 оператор")
+		for in := 0; in < len(availbleOperators); in++ {
+			if arr[i] == availbleOperators[in] {
+				isOperator = true
+				operator = arr[i]
+				//fmt.Printf("arr[i] = %v, oper = %v\n", arr[i], availbleOperators[in])
+				arr = strings.Split(strings.Join(arr, ""), arr[i])
+				if len(arr) == 2 {
+					operand1 = arr[0]
+					operand2 = arr[1]
+					if operand1 == "" || operand2 == "" {
+						panic("Ошибка! Операнды не могут отсутсвовать!")
+					}
+				} else {
+					panic("Ошибка! Введите 2 операнда и 1 оператор")
+				}
 			}
-			isOperator = true
-		case "-":
-			operator = arr[i]
-			arr = strings.Split(strings.Join(arr, ""), arr[i])
-			if len(arr) == 2 {
-				operand1 = arr[0]
-				operand2 = arr[1]
-			} else {
-				panic("Ошибка! Введите 2 операнда и 1 оператор")
-			}
-			isOperator = true
-		case "*":
-			operator = arr[i]
-			arr = strings.Split(strings.Join(arr, ""), arr[i])
-			if len(arr) == 2 {
-				operand1 = arr[0]
-				operand2 = arr[1]
-			} else {
-				panic("Ошибка! Введите 2 операнда и 1 оператор")
-			}
-			isOperator = true
-		case "/":
-			operator = arr[i]
-			arr = strings.Split(strings.Join(arr, ""), arr[i])
-			if len(arr) == 2 {
-				operand1 = arr[0]
-				operand2 = arr[1]
-			} else {
-				panic("Ошибка! Введите 2 операнда и 1 оператор")
-			}
-			isOperator = true
 		}
 	}
 	if isOperator {
